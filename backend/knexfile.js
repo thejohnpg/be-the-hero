0536883e18fdf89beaf52670aadeec3f9e098f11
John Peterson <thejohnpg@gmail.com>
2020-03-27 19:38:29 -0300

    chore: add unit tests to the backend

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,46 +1,55 @@
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './src/database/db.sqllite'
+      filename: "./src/database/db.sqllite"
     },
     migrations: {
-      directory: './src/database/migrations'
+      directory: "./src/database/migrations"
     },
-    useNullAsDefault:true,
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/teste.sqllite"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
